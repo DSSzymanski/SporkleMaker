f003a2f6c9de9393e3b95d46cf2b1c0fb0350fcd
DSSzymanski <dszymanski@umass.edu>
2020-05-27 18:38:40 -0400

    changed web scraping from OOP oriented in LyricGetter to procedural based programming in WebScraper.py. WebScraper does same exact thing as LyricGetter but without a needless class.

--- LyricGetter.py
+++ LyricGetter.py
@@ -2,53 +2,63 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 class LyricGetter:
-	def __init__(self, url):
-		"""
-		:param url: url from Lyrics.com
-		#TODO validate URL
-		#TODO error handling:
-		   if page isn't found
-		   if page structure changes
-   		"""
-		self.url = url
-		self.soup = self._get_soup()
-		#container to store lyrics word by word
-		self.lyrics = []
-		#initializes info about song. Title, artist, lyrics
-		self._init_title()
-		self._init_artist()
-		self._init_lyrics()
-	def _get_soup(self):
-		"""initialize beautiful soup object with URL"""
-		req = urllib.request.Request(self.url)
-		response = urllib.request.urlopen(req)
-		return BeautifulSoup(response, 'html.parser')
-	def _init_title(self):
-		"""scrape website for song title and store in class var"""
-		self.title = self.soup.find_all(id='lyric-title-text')[0].string
-	def _init_artist(self):
-		"""scrape website for song artist and store in class var"""
-		self.artist = self.soup.find_all('h3', 'lyric-artist')[0].contents[0].string
-	def _init_lyrics(self):
-		"""scrape website for song lyrics and store in class container"""
-		raw = self.soup.find_all(id='lyric-body-text')[0].contents
-		for line in raw:
-			#line below eliminates <a> tags in lyrics where
-			#the words are linked with definitions
-			line = line.string
-			self.lyrics += line.split()
-	def get_lyrics(self):
-		"""Return the lyrics of song in []"""
-		return self.lyrics
-	def get_artist(self):
-		"""Return the artist(str) of song"""
-		return self.artist
-	def get_title(self):
-		"""Return the title(str) of song"""
-		return self.title
+    def __init__(self, url):
+        """
+        :param url: url from Lyrics.com
+        #TODO: validate URL
+        #TODO: error handling:
+           1. if page isn't found
+           2. if page structure changes
+        """
+        self.url = url
+        self.soup = self._get_soup()
+        #container to store lyrics word by word
+        self.lyrics = []
+        #initializes info about song. Title, artist, lyrics
+        self._init_title()
+        self._init_artist()
+        self._init_lyrics()
+
+    def _get_soup(self):
+        """Initialize beautiful soup object with URL"""
+        req = urllib.request.Request(self.url)
+        response = urllib.request.urlopen(req)
+        return BeautifulSoup(response, 'html.parser')
+
+    def _init_title(self):
+        """Scrape website for song title and store in class var"""
+        self.title = self.soup.find_all(id='lyric-title-text')[0].string
+
+    def _init_artist(self):
+        """Scrape website for song artist and store in class var"""
+        self.artist = self.soup.find_all('h3', 'lyric-artist')[0].contents[0].string
+
+    def _init_lyrics(self):
+        """Scrape website for song lyrics and store in class container"""
+        raw = self.soup.find_all(id='lyric-body-text')[0].contents
+        for line in raw:
+            """
+            line below eliminates <a> tags in lyrics where
+            the words are linked with definitions
+            """
+            line = line.string
+
+            self.lyrics += line.split()
+
+    def get_lyrics(self):
+        """Return the lyrics of song in []"""
+        return self.lyrics
+
+    def get_artist(self):
+        """Return the artist(str) of song"""
+        return self.artist
+
+    def get_title(self):
+        """Return the title(str) of song"""
+        return self.title
+
 #for testing purposes
-#TODO remove after class is complete
+#TODO: remove after class is complete
 if __name__ == "__main__":
-	url = "https://www.lyrics.com/lyric/36440596/Childhood%E2%80%99s+End"
-	LG = LyricGetter(url)
-	
+    url = "https://www.lyrics.com/lyric/36440596/Childhood%E2%80%99s+End"
+    LG = LyricGetter(url)
---
+++ WebScraper.py
@@ -0,0 +1,67 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+def run(url):
+    """
+    :param url: url from Lyrics.com
+    #TODO: validate URL
+    #TODO: error handling:
+       1. if page isn't found
+       2. if page structure changes
+    """
+    soup = get_soup(url)
+    title = get_title(soup)
+    artist = get_artist(soup)
+    lyrics = get_lyrics(soup)
+    return [title, artist, lyrics]
+
+def get_soup(url):
+    """
+    :param url: string object of url to Lyrics.com song lyric page
+
+    Initialize and return beautiful soup object with URL
+    """
+    req = urllib.request.Request(url)
+    response = urllib.request.urlopen(req)
+    return BeautifulSoup(response, 'html.parser')
+
+def get_title(soup):
+    """
+    :param url: string object of url to Lyrics.com song lyric page
+
+    Scrape website for song title and return it
+    """
+    return soup.find_all(id='lyric-title-text')[0].string
+
+def get_artist(soup):
+    """
+    :param url: string object of url to Lyrics.com song lyric page
+
+    Scrape website for song artist and return it
+    """
+    return soup.find_all('h3', 'lyric-artist')[0].contents[0].string
+
+def get_lyrics(soup):
+    """
+    :param url: string object of url to Lyrics.com song lyric page
+
+    Scrape website for song lyrics and return it
+    """
+    lyrics = []
+    raw = soup.find_all(id='lyric-body-text')[0].contents
+    for line in raw:
+        """
+        line below eliminates <a> tags in lyrics where
+        the words are linked with definitions
+        """
+        line = line.string
+
+        lyrics += line.split()
+
+    return lyrics
+
+#for testing purposes
+#TODO: remove after class is complete
+if __name__ == "__main__":
+    url = "https://www.lyrics.com/lyric/36440596/Childhood%E2%80%99s+End"
+    print(run(url))
